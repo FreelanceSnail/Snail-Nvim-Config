2dbcc85649389c8febd3cd449f92cc48b06dd7c4
lvxj <lvxj@lvxj-Desktop>
2026-06-22 16:54:22 +0800

    fix: 暴露 NvimTree 命令供手动调用

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"nvim-tree/nvim-tree.lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = { "NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus", "NvimTreeFindFile" },
 		keys = {
 			{ "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "File tree" },
 		},
