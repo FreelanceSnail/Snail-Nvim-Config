af5e03c5d4195610a219ada87a1b2ad906589de3
lvxj <lvxj@lvxj-Desktop>
2026-06-22 16:58:51 +0800

    fix: 修正 vlime 快捷键，使用 <leader> 作为 leader

--- lua/plugins/commonlisp.lua
+++ lua/plugins/commonlisp.lua
@@ -5,6 +5,8 @@ return {
     init = function(plugin)
       -- vlime 的 Vim 插件文件位于仓库的 vim/ 子目录
       vim.opt.rtp:append(plugin.dir .. "/vim")
+      -- 让 vlime 的默认快捷键也使用 <Space> 作为前缀
+      vim.g.vlime_leader = "<leader>"
     end,
     config = function()
       -- 启用自动文档与包名显示
@@ -19,11 +21,17 @@ return {
         end,
       })
 
-      -- 自定义快捷键
-      local opts = { noremap = true, silent = true }
-      vim.keymap.set("n", "<leader>rr", "<cmd>VlimeConnect<CR>", vim.tbl_extend("force", opts, { desc = "Connect to SWANK" }))
-      vim.keymap.set("n", "<leader>rf", "<cmd>VlimeLoadFile<CR>", vim.tbl_extend("force", opts, { desc = "Load current Lisp file" }))
-      vim.keymap.set("n", "<leader>rq", "<cmd>VlimeClose<CR>", vim.tbl_extend("force", opts, { desc = "Close SWANK connection" }))
+      -- 自定义便捷快捷键（vlime 默认用 <LocalLeader>，上面已改为 <leader>）
+      local opts = { noremap = true, silent = true, buffer = true }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "lisp" },
+        callback = function()
+          vim.keymap.set("n", "<leader>rr", ":call vlime#plugin#ConnectREPL()<CR>", vim.tbl_extend("force", opts, { desc = "Connect to SWANK" }))
+          vim.keymap.set("n", "<leader>rf", ":call vlime#plugin#CompileFile(expand('%:p'))<CR>", vim.tbl_extend("force", opts, { desc = "Compile current Lisp file" }))
+          vim.keymap.set("n", "<leader>rq", ":call vlime#plugin#CloseCurConnection()<CR>", vim.tbl_extend("force", opts, { desc = "Close SWANK connection" }))
+          vim.keymap.set("n", "<leader>re", ":call vlime#plugin#SendToREPL(vlime#ui#CurTopExpr())<CR>", vim.tbl_extend("force", opts, { desc = "Send top expr to REPL" }))
+        end,
+      })
     end,
   },
 }
